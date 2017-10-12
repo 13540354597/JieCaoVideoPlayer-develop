35131f82bdce5d75dab6824c0a6c093375974c15
ZDEAM <1241577948@qq.com>
2017-10-12 12:52:40 +0800

    Initial commit

---
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/practice/Test.java
@@ -0,0 +1,118 @@
+package fm.jiecao.jiecaovideoplayer.practice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.squareup.picasso.Picasso;
+
+import fm.jiecao.jcvideoplayer_lib.JCUserAction;
+import fm.jiecao.jcvideoplayer_lib.JCUserActionStandard;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
+import fm.jiecao.jiecaovideoplayer.CustomView.JCVideoPlayerStandardShowShareButtonAfterFullscreen;
+import fm.jiecao.jiecaovideoplayer.CustomView.MyJCVideoPlayerStandard;
+import fm.jiecao.jiecaovideoplayer.MainActivity;
+import fm.jiecao.jiecaovideoplayer.R;
+
+/**
+ * Created by TR 105 on 2017/8/9.
+ */
+
+public class Test extends AppCompatActivity {
+
+    fm.jiecao.jiecaovideoplayer.practice.JCVideoPlayerStandard myJCVideoPlayerStandard;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.test);
+
+        initView();
+    }
+
+    private void initView() {
+        myJCVideoPlayerStandard = (fm.jiecao.jiecaovideoplayer.practice.JCVideoPlayerStandard) findViewById(R.id.jc_video);
+        myJCVideoPlayerStandard.setUp("http://video.jiecao.fm/11/23/xu/%E5%A6%B9%E5%A6%B9.mp4"
+                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "Test");
+        Picasso.with(this)
+                .load("http://img4.jiecaojingxuan.com/2016/11/23/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png@!640_360")
+                .into(myJCVideoPlayerStandard.thumbImageView);
+
+        JCVideoPlayer.setJcUserAction(new MyUserActionStandard());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        JCVideoPlayer.releaseAllVideos();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (JCVideoPlayer.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    class MyUserActionStandard implements JCUserActionStandard {
+
+        @Override
+        public void onEvent(int type, String url, int screen, Object... objects) {
+            switch (type) {
+                case JCUserAction.ON_CLICK_START_ICON:
+                    Log.i("USER_EVENT", "ON_CLICK_START_ICON" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_START_ERROR:
+                    Log.i("USER_EVENT", "ON_CLICK_START_ERROR" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_START_AUTO_COMPLETE:
+                    Log.i("USER_EVENT", "ON_CLICK_START_AUTO_COMPLETE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_PAUSE:
+                    Log.i("USER_EVENT", "ON_CLICK_PAUSE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_RESUME:
+                    Log.i("USER_EVENT", "ON_CLICK_RESUME" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_SEEK_POSITION:
+                    Log.i("USER_EVENT", "ON_SEEK_POSITION" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_AUTO_COMPLETE:
+                    Log.i("USER_EVENT", "ON_AUTO_COMPLETE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_ENTER_FULLSCREEN:
+                    Log.i("USER_EVENT", "ON_ENTER_FULLSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_QUIT_FULLSCREEN:
+                    Log.i("USER_EVENT", "ON_QUIT_FULLSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_ENTER_TINYSCREEN:
+                    Log.i("USER_EVENT", "ON_ENTER_TINYSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_QUIT_TINYSCREEN:
+                    Log.i("USER_EVENT", "ON_QUIT_TINYSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_TOUCH_SCREEN_SEEK_VOLUME:
+                    Log.i("USER_EVENT", "ON_TOUCH_SCREEN_SEEK_VOLUME" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_TOUCH_SCREEN_SEEK_POSITION:
+                    Log.i("USER_EVENT", "ON_TOUCH_SCREEN_SEEK_POSITION" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+
+                case JCUserActionStandard.ON_CLICK_START_THUMB:
+                    Log.i("USER_EVENT", "ON_CLICK_START_THUMB" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserActionStandard.ON_CLICK_BLANK:
+                    Log.i("USER_EVENT", "ON_CLICK_BLANK" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                default:
+                    Log.i("USER_EVENT", "unknow");
+                    break;
+            }
+        }
+    }
+}
